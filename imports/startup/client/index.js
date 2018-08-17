2d4491daf8dadbcf122368a68e60b93dec1318d9
Diego Robles <diego@hemployeed.com>
2018-08-16 22:28:17 -0300

    add react

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,5 +1,7 @@
 import { Meteor } from "meteor/meteor";
+import React from "react";
+import { render } from "react-dom";
 
 Meteor.startup(() => {
-  console.log("client initialized");
+  render(<h1>Hello, react!</h1>, document.getElementById("react-root"));
 });
